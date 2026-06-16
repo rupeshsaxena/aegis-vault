89511d1e51779c83cb511777ce5177fd482706d5
Rupesh Saxena <rupesh.saxena20@gmail.com>
2026-06-16 20:33:31 +0530

    chore: initialize AegisVault repository structure

--- packages/SecureVaultKit/Sources/SecureVaultKit/Protocols/SecureVaultErrors.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public enum SecureVaultError: Error, Equatable, Sendable {
-    case vaultNotFound(VaultID)
-    case itemNotFound(VaultItemID)
-    case blobNotFound(BlobID)
-    case locked
-    case unsupportedOperation(String)
-}
